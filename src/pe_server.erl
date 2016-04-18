828de9602d6ba5cd38ccf0ab9ca53d7076749cfa
IgorMuravev <to.imuravev@gmail.com>
2016-04-18 22:11:44 +0600

    first moves!

--- src/pe_server.erl
+++ src/pe_server.erl
@@ -1,29 +1,43 @@
 -module(pe_server).
--export([start/0, stop/0]).
-
+%%-export([start/0, stop/0]).
+-compile(export_all).
 server_logfile() -> "srv.log".
 
 server_log() -> file:open(server_logfile(), [append]).
 
-
+notify(_,[],_)->ok;
+notify(ClientPid, [ClientPid|T] , Msg) ->
+	notify(ClientPid, T, Msg);
+notify(ClientPid, [H | T], Msg) ->
+	H ! Msg,
+	notify(ClientPid, T, Msg). 
 
 server_loop1(Clients, LogDevice) ->
 	receive
 		{cmd,{connect,ClientPid,empty}} ->
 			io:fwrite(LogDevice, "[~w] Connected process ~w~n",[calendar:local_time(),ClientPid]),
 			server_loop1([ClientPid | Clients],LogDevice);
-		{cmd,{break,empty}} ->
-			io:fwrite(LogDevice, "[~w] Server stopped ~n",[calendar:local_time()]);
+		{cmd,{break,empty,empty}} ->
+			io:fwrite(LogDevice, "[~w] Server stopped ~n",[calendar:local_time()]),
+			file:close(LogDevice);
+		{cmd, {msg,ClientPid,Msg}} ->
+			io:fwrite(LogDevice, "[~w] Received message ~n",[calendar:local_time()]),
+			notify(ClientPid, Clients, Msg),
+			server_loop1(Clients, LogDevice);
 		Uncaught ->
 			io:fwrite(LogDevice, "[~w] Uncaught message ~w~n",[calendar:local_time(), Uncaught]),
 			server_loop1(Clients,LogDevice)
 	end.
+
 server_loop2(Clients) ->
 	receive
 		{cmd,{connect,ClientPid,empty}} ->
 			server_loop2([ClientPid | Clients]);
-		{cmd,{break,empty}} ->
+		{cmd,{break,empty,empty}} ->
 			ok;
+		{cmd, {msg,ClientPid,Msg}} ->
+			notify(ClientPid, Clients,Msg),
+			server_loop2(Clients);
 		Uncaught ->
 			server_loop2(Clients)
 	end.
@@ -39,6 +53,4 @@ start()->
 	end.
 
 stop()->
-	global:send(srv, {cmd,{break, empty}}).
-
-
+	global:send(srv, {cmd,{break,empty,empty}}).
